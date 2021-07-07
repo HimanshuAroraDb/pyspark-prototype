74c60f8742ef54ee503cfac60eabb74775f4f5a7
Amine <amine.benhamza@gmail.com>
2021-07-07 11:43:19 +0200

    putting back both imports

--- tests/test_etl_job.py
+++ tests/test_etl_job.py
@@ -15,6 +15,7 @@ from pyspark.sql.functions import mean
 from dependencies.spark import start_spark
 from jobs.etl_job import transform_data
 from pyspark.sql import SparkSession
+from pyspark import  SQLContext
 
 class SparkETLTests(unittest.TestCase):
     """Test suite for transformation in etl_job.py
